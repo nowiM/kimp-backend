46328048af52461d2e7957784362b2fca784c7cc
KimDoHyeon <ghkdtprhf5@naver.com>
2024-09-10 20:32:10 +0900

    feat(client): CoinFilterArea css 작업

--- client/src/components/coinFilterArea/CoinFilterArea.js
+++ client/src/components/coinFilterArea/CoinFilterArea.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import formatUpbitPrice from '../../modules/formatUpbitPrice';
 import formatRate from '../../modules/formatRate.js';
 import updatePremium from '../../modules/updatePremium.js';
@@ -10,7 +10,6 @@ import bybitLogo from '../images/logo_bybit.png';
 import twoWayArrow from '../images/twoWayArrow.svg'
 
 const CoinFilterArea = ({ coin, data, exchangeRate }) => {
-  console.log(data)
   if (!data) return null;
   const { premiumClass, premiumValue, premiumRate } = updatePremium(coin, data, exchangeRate);
 
@@ -35,35 +34,43 @@ const CoinFilterArea = ({ coin, data, exchangeRate }) => {
         </div>
       </div>
       <div className="coins">
-        <img src= {`https://static.upbit.com/logos/${coin}.png`} alt="" />
+        <div className="top">
+          <div className="logoAndName">
+            <img className='logo' src= {`https://static.upbit.com/logos/${coin}.png`} alt="" />
+            <span className="name">{coin}</span>
+          </div>
 
-        <div className="upbitPrice">
-          업비트
-          {formatUpbitPrice(data.upbitPrice)}
+          <div className="upbitPrice">
+            <span className="title">업비트</span>
+            {formatUpbitPrice(data.upbitPrice)}
+          </div>
+          <div className="symbol">-</div>
+          <div className="bybitPrice">
+          <span className="title">바이비트</span>
+            {formatUpbitPrice(data.bybitPrice * exchangeRate)}
+          </div>
+          <div className="symbol">=</div>
+          <div className="kimpPremium">
+          <span className="title">김치 프리미엄</span>
+            <span className={premiumClass}>{formatUpbitPrice(premiumValue)} ({formatRate(premiumRate)}%)</span>
+          </div>
         </div>
 
-        <div className="bybitPrice">
-          바이비트
-          {formatUpbitPrice(data.bybitPrice * exchangeRate)}
-        </div>
+        <div className="bottom">
+          <div className="signedChangeRate">
+          <span className="title">등락</span>
+            {data.signedChangeRate > 0 ? `+${formatRate(Math.floor(data.signedChangeRate * 10000) / 100)}%` : `${formatRate(Math.floor(data.signedChangeRate * 10000) / 100)}%`}
+          </div>
 
-        <div className="signedChangeRate">
-          등락
-          {data.signedChangeRate > 0 ? `+${formatRate(Math.floor(data.signedChangeRate * 10000) / 100)}%` : `${formatRate(Math.floor(data.signedChangeRate * 10000) / 100)}%`}
-        </div>
+          <div className="lowest52WeekPrice">
+          <span className="title">52주 최저</span>
+          {formatUpbitPrice(data.lowest_52_week_price)}
+          </div>
 
-        <div className="lowest52WeekPrice">
-          52주 최저
-        {formatUpbitPrice(data.lowest_52_week_price)}
-        </div>
-
-        <div className="accTradePrice">
-          거래량
-          {Math.floor(data.acc_trade_price_24h / 100000000)}억
-        </div>
-        <div className="kimpPremium">
-          김프
-          <span className={premiumClass}>{formatUpbitPrice(premiumValue)} ({formatRate(premiumRate)}%)</span>
+          <div className="accTradePrice">
+          <span className="title">거래대금</span>
+            {Math.floor(data.acc_trade_price_24h / 100000000)}억
+          </div>
         </div>
       </div>
     </div>
